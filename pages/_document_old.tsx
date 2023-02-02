952d421a1995062f1604231c85904d5d1127e9b1
markwinap <markwinap@gmail.com>
2023-02-01 23:33:03 -0600

    Update

---
+++ pages/_document_old.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+      <Html lang="en" className={roboto.className}>
+        <Head>
+          {/* PWA primary color */}
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name="emotion-insertion-point" content="" />
+          {(this.props as any).emotionStyleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+  )
+}
